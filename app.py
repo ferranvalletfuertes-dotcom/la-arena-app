ccd8b5471a23945f81dca01eef2723ca549611a8
ferranvalletfuertes-dotcom <ferranvalletfuertes@gmail.com>
2026-08-18 21:57:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta, timezone
 import streamlit.components.v1 as components
 from supabase import create_client, Client
 
-LOGO_URL = "https://i.imgur.com/UV7wAVV.png"
+LOGO_URL = "https://github.com/ferranvalletfuertes-dotcom/la-arena-app/blob/bf3d6cbbefd8c607b74d0e6b3a7518308e2c6d9f/logo.png"
 
 st.set_page_config(page_title="Modo Combate | La Arena", page_icon=LOGO_URL, layout="centered")
 
