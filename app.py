0bee44c52b1093cab2cc9beccdc362940fa059cb
ferranvalletfuertes-dotcom <ferranvalletfuertes@gmail.com>
2026-08-22 22:18:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from supabase import create_client, Client
 # --- IMPORTACIONES MODULARES (TU NUEVO EJÉRCITO) ---
 from datos import pildoras, MISIONES_DESARROLLO, MISIONES_FISICAS, MISIONES_SOCIALES, MISIONES_MENTALES, MISIONES_ORDEN
 from motor import get_rank_info, calcular_rango, calcular_riesgo_recompensa, generar_codigo_sala, tiene_boost_activo
-from idiomas import DIC
 from interfaz import cargar_css, generar_carta_html, generar_html_mision, render_navbar, render_top_bar
 
 LOGO_URL = "https://raw.githubusercontent.com/ferranvalletfuertes-dotcom/la-arena-app/main/logo.png"
